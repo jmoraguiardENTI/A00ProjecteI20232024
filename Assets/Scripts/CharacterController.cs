eb0d8eaa7b965e1fb70c95535978565e4b52ccf8
Joan Mora <joanmora@enti.cat>
2024-02-19 14:00:24 +0100

    Add character movement and jump

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
+    public float movementSpeed = 5.0f;
+    public float jumpForce = 100.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,22 @@ public class CharacterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-movementSpeed, 0);
+            //GetComponent<Rigidbody2D>().AddForce(new Vector2(-1, 0));
+            //transform.position += new Vector3(-1, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(movementSpeed, 0);
+            //GetComponent<Rigidbody2D>().AddForce(new Vector2(1, 0));
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+        }
     }
 }
